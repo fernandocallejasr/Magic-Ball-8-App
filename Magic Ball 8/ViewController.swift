2eb439bf2075b3ba0955b7d1588930993f09c5a8
Fernando Callejas <ferd.callejas@gmail.com>
2020-07-16 19:56:40 -0500

    Initial Commit

---
+++ Magic Ball 8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic Ball 8
+//
+//  Created by user177767 on 7/16/20.
+//  Copyright © 2020 Fernando Callejas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
